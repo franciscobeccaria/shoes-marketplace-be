1990105254721863c2df32aea18048aa4a0a5607
Francisco Beccaria <fbeccaria24@gmail.com>
2024-09-25 12:50:03 -0300

    chore: add simple GET route for testing

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,4 +9,10 @@ export class ProductsController {
   async scrapeProducts(@Query('storeUrl') storeUrl: string): Promise<any> {
     return this.productsService.scrapeProducts(storeUrl);
   }
+
+  // Simple GET route for testing
+  @Get('test')
+  async getTest(): Promise<any> {
+    return { message: 'App is running correctly!' };
+  }
 }

99652b06802b78927d75c2708f05388d841bcf42
Francisco Beccaria <fbeccaria24@gmail.com>
2024-09-25 12:41:03 -0300

    chore: update gitignore and puppeteer browser options

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,7 +4,10 @@ import * as puppeteer from 'puppeteer';
 @Injectable()
 export class ProductsService {
   async scrapeProducts(storeUrl: string): Promise<any> {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
     await page.goto(storeUrl);
